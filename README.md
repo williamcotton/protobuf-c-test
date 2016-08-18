c1cd0b11e87b578e685dabf220d15b1cceeb1ef2
William Cotton <williamcotton@gmail.com>
2016-08-18 10:01:02 -0500

    updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # protobuf-c-test
+
+## TODO
+
+http://stackoverflow.com/questions/65820/unit-testing-c-code
--- test/amessage-spec.js
+++ test/amessage-spec.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const exec = require('child_process').exec
 
-test('1-1-hello', function (t) {
+test('amessage_serialize piped to amessage_deserialize', function (t) {
   exec('./build/amessage_serialize 10 2 | ./build/amessage_deserialize', (err, stdout, stderr) => {
     if (err) {}
     t.equal(stdout, 'Received: a=10  b=2\n', 'stdout matches')
